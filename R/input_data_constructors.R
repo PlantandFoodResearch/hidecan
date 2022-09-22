af2a5c4a91e1f09ea5f292bea160d4367c212655
Olivia Angelin-Bonnet <olivia.angelinbonnet@gmail.com>
2022-09-23 08:18:41 +1200

    Simplified input GWAS and DE data

--- R/input_data_constructors.R
+++ R/input_data_constructors.R
@@ -2,16 +2,13 @@
 #'
 #' @param dat Tibble, results from a GWAS analysis, with at least columns
 #' `chromosome`, `position` and `score`.
-#' @param title Character, the name of the analysis. Default value is `"GWAS"`.
 #' @returns A `GWAS_data` object, i.e. a tibble.
 #' @export
-new_GWAS_data <- function(dat, title = "GWAS"){
+new_GWAS_data <- function(dat){
   ## Making sure that the input is a tibble
   stopifnot(tibble::is_tibble(dat))
-  stopifnot(is.character(title))
 
   class(dat) <- c("GWAS_data", class(dat))
-  attr(dat, "title") <- title
 
   dat
 }
@@ -38,29 +35,26 @@ validate_GWAS_data <- function(x){
 #'
 #' * `chromosome`: character column, chromosome on which the marker is located.
 #'
-#' * `position`: numeric, the physical position of the marker along the chromosome.
+#' * `position`: numeric, the physical position of the marker along the chromosome (in bp).
 #'
 #' * `score`: numeric, the GWAS score of the marker.
 #'
 #' @param dat Tibble, results from a GWAS analysis. See Details.
-#' @param title Character, the name of the analysis. Default value is `"GWAS"`.
 #' @param keep_rownames_as Character, the name of the column in which to save the
 #' rownames of the input data-frame. Default value is `NULL`, i.e. rownames will
 #' be discarded.
 #' @returns A `GWAS_data` object, i.e. a tibble.
 #' @export
-GWAS_data <- function(dat, title = "GWAS", keep_rownames_as = NULL){
+GWAS_data <- function(dat, keep_rownames_as = NULL){
   ## If the data is not a tibble, transform it
   if(!tibble::is_tibble(dat)){
 
     dat <- tibble::as_tibble(dat, rownames = keep_rownames_as)
   }
 
-  dat <- new_GWAS_data(dat, title)
+  dat <- new_GWAS_data(dat)
   validate_GWAS_data(dat)
 }
-
-
 
 
 
@@ -68,17 +62,14 @@ GWAS_data <- function(dat, title = "GWAS", keep_rownames_as = NULL){
 #' DE_data constructor
 #'
 #' @param dat Tibble, results from a differential expression analysis, with at least columns
-#' `chromosome`, `score` and either `position` or both `start` and `end`.
-#' @param title Character, the name of the analysis. Default value is `"DE"`.
+#' `chromosome`, `score`, `start` and `end`.
 #' @returns A `DE_data` object, i.e. a tibble.
 #' @export
-new_DE_data <- function(dat, title = "DE"){
+new_DE_data <- function(dat){
   ## Making sure that the input is a tibble
   stopifnot(tibble::is_tibble(dat))
-  stopifnot(is.character(title))
 
   class(dat) <- c("DE_data", class(dat))
-  attr(dat, "title") <- title
 
   dat
 }
@@ -90,21 +81,10 @@ new_DE_data <- function(dat, title = "DE"){
 validate_DE_data <- function(x){
 
   ## A GWAS result table must at least contain these columns
-  .check_cols(x, c("chromosome", "score"))
-
-  if(!( ("position" %in% colnames(x)) | all(c("start", "end") %in% colnames(x)) )){
-    stop("Input data-frame should contain either a 'position' column or both a 'start' and 'end' columns.")
-  }
-
-  if("position" %in% colnames(x)){
-    if(!is.numeric(x[["position"]])) stop("'position' column should contain numeric values.", call. = FALSE)
-
-    if(all(c("start", "end") %in% colnames(x))) warning("'position' column takes precedence over 'start' and 'end' columns.")
-  } else {
-    if(!is.numeric(x[["start"]]) | !is.numeric(x[["end"]])) stop("'start' and 'end' columns should contain numeric values.", call. = FALSE)
-  }
+  .check_cols(x, c("chromosome", "start", "end", "score"))
 
 
+  if(!is.numeric(x[["start"]]) | !is.numeric(x[["end"]])) stop("'start' and 'end' columns should contain numeric values.", call. = FALSE)
   if(!is.numeric(x[["score"]])) stop("'score' column should contain numeric values.", call. = FALSE)
 
   x
@@ -119,18 +99,23 @@ validate_DE_data <- function(x){
 #'
 #' * `score`: numeric, the DE score of the gene/transcript.
 #'
-#' In addition, the input data should have either:
+#' * `start`: numeric, starting position of the gene/transcript (in bp).
 #'
-#' * a `start` and an `end` column: numeric, start and end positions of the gene/transcript (in Mb).
-#'
-#' * a `position` column: numeric, the position of the gene/transcript (in Mb). Could represent
-#' the middle of the gene or transcript.
+#' * `end`: numeric, end position of the gene/transcript (in bp).
 #'
 #' @param dat Tibble, results from a GWAS analysis. See Details.
-#' @param title Character, the name of the analysis. Default value is `"GWAS"`.
 #' @param keep_rownames_as Character, the name of the column in which to save the
 #' rownames of the input data-frame. Default value is `NULL`, i.e. rownames will
 #' be discarded.
-#' @returns A `GWAS_data` object, i.e. a tibble.
+#' @returns A `DE_data` object, i.e. a tibble.
 #' @export
+DE_data <- function(dat, keep_rownames_as = NULL){
+  ## If the data is not a tibble, transform it
+  if(!tibble::is_tibble(dat)){
 
+    dat <- tibble::as_tibble(dat, rownames = keep_rownames_as)
+  }
+
+  dat <- new_DE_data(dat)
+  validate_DE_data(dat)
+}
--- tests/testthat/test-input_data_constructors.R
+++ tests/testthat/test-input_data_constructors.R
@@ -3,17 +3,13 @@ test_that("new_GWAS_data works", {
   data <- tibble::tibble(chromosome = LETTERS[1:5], position = 1:5, score = 1:5)
 
   expect_error(new_GWAS_data(as.data.frame(data)))
-  expect_error(new_GWAS_data(data, 2))
 
   res <- new_GWAS_data(data)
 
-  expect_equal(unclass(res), unclass(data), ignore_attr = TRUE)
+  expect_equal(unclass(res), unclass(data))
 
   expect_equal(class(res)[1], "GWAS_data")
-  expect_equal(attr(res, "title"), "GWAS")
 
-  res <-  new_GWAS_data(data, "test")
-  expect_equal(attr(res, "title"), "test")
 })
 
 
@@ -26,7 +22,7 @@ test_that("GWAS_data works", {
 
   expect_error(GWAS_data(data), NA)
 
-  expect_equal(unclass(GWAS_data(data)), unclass(data), ignore_attr = TRUE)
+  expect_equal(unclass(GWAS_data(data)), unclass(data))
 
   data_2 <- as.data.frame(data)
   rownames(data_2) <- letters[1:5]
@@ -42,18 +38,35 @@ test_that("GWAS_data works", {
 
 test_that("new_DE_data works", {
 
-  data <- tibble::tibble(chromosome = LETTERS[1:5], position = 1:5, score = 1:5)
+  data <- tibble::tibble(chromosome = LETTERS[1:5], start = 1:5, end = 2:6, score = 1:5)
 
   expect_error(new_DE_data(as.data.frame(data)))
   expect_error(new_DE_data(data, 2))
 
   res <- new_DE_data(data)
 
-  expect_equal(unclass(res), unclass(data), ignore_attr = TRUE)
+  expect_equal(unclass(res), unclass(data))
 
   expect_equal(class(res)[1], "DE_data")
-  expect_equal(attr(res, "title"), "DE")
+})
 
-  res <-  new_DE_data(data, "test")
-  expect_equal(attr(res, "title"), "test")
+test_that("DE_data works", {
+
+  data <- tibble::tibble(chromosome = LETTERS[1:5], start = 1:5, end = 2:6, score = 1:5)
+
+  expect_error(DE_data(tibble::tibble(a = LETTERS[1:5], b = 1:5, score = 1:5)),
+               "Input data-frame is missing the following columns: 'chromosome', 'start', 'end'.")
+
+  expect_error(DE_data(data), NA)
+
+  expect_equal(unclass(DE_data(data)), unclass(data))
+
+  data_2 <- as.data.frame(data)
+  rownames(data_2) <- letters[1:5]
+
+  res <- DE_data(data_2)
+  expect_equal(colnames(res),  colnames(data_2))
+
+  res <- DE_data(data_2, keep_rownames_as = "rowname")
+  expect_equal(res[["rowname"]], rownames(data_2))
 })
