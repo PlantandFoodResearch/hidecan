a8879b165ffa120c4fb5fc1d4f20b06d115ca3c4
Olivia Angelin-Bonnet <Olivia.Angelin-Bonnet@plantandfood.co.nz>
2022-11-11 16:51:47 +1300

    A somewhat functional app with all plot options

--- inst/shiny_apps/hidecan_shiny.R
+++ inst/shiny_apps/hidecan_shiny.R
@@ -73,6 +73,24 @@ get_input_data_reactive <- function(input_id, func){
 }
 
 
+render_custom_names_textInput <- function(input_id, suffix){
+  substitute(
+    renderUI({
+      req(input$input_id)
+
+      input_files_df <- input$input_id
+
+      purrr::map(seq_len(nrow(input_files_df)),
+                 ~ textInput(inputId = paste0("custom_name_", suffix, "_", .x),
+                             label = input_files_df$name[.x],
+                             value = " "))
+
+    })
+
+  )
+}
+
+
 gwas_threshold_tabs <- tabsetPanel(
   id = "thr_selection_gwas",
   type = "hidden",
@@ -150,7 +168,12 @@ ui <- fluidPage(
           fileInput("upload_can",
                     "Upload one or more candidate genes list file",
                     accept = ".csv",
-                    multiple = TRUE)
+                    multiple = TRUE),
+
+          ## Choose custom names for the different input files
+          uiOutput("input_custom_names_gwas"),
+          uiOutput("input_custom_names_de"),
+          uiOutput("input_custom_names_can")
         ),
 
         tabPanel(
@@ -163,9 +186,41 @@ ui <- fluidPage(
           de_threshold_tabs
         ),
 
+        tabPanel(
+          title = "Plot options",
+
+          checkboxInput("colour_genes_by_score", "Colour genes by score?", value = TRUE),
+          textInput("title", "Title"),
+          textInput("subtitle", "Subtitle"),
+          fluidRow(
+            column(6,
+                   numericInput("nrows", "Number of rows", value = NULL, min = 1, max = Inf)),
+            column(6,
+                   numericInput("ncols", "Number of columns", value = 2, min = 1, max = Inf))
+          ),
+          selectInput("legend_position", "Legend position", c("bottom", "top", "left", "right", "none")),
+          fluidRow(
+            column(6,
+                   numericInput("point_size", "Point size", value = 3, min = 0, max = Inf))
+          ),
+          fluidRow(
+            column(6,
+                   numericInput("label_size", "Label size", value = 3.5, min = 0, max = Inf)),
+            column(6,
+                   numericInput("label_padding", "Label padding", value = 0.15, min = 0, max = Inf))
+          ),
+        ),
+
+        tabPanel(
+          title = "Download",
+
+          uiOutput("download_params")
+        )
+
+
       ),
 
-      uiOutput("download_params")
+      #uiOutput("download_params")
     ),
 
     ## ---------------------------------------------------------------------- ##
@@ -204,6 +259,23 @@ server <- function(input, output, session){
 
   ## Reading in can_data
   can_data_list <- eval(get_input_data_reactive("upload_can", CAN_data))
+
+
+  ## Adding the buttons for the custom names
+  output$input_custom_names_gwas <- eval(render_custom_names_textInput("upload_gwas", "gwas"))
+  output$input_custom_names_de <- eval(render_custom_names_textInput("upload_de", "de"))
+  output$input_custom_names_can <- eval(render_custom_names_textInput("upload_can", "can"))
+
+  get_custom_names <- reactive({
+    c(
+      purrr::map(seq_along(gwas_data_list()),
+                 ~ eval(str2expression(paste0("input$custom_name_gwas_", .x)))),
+      purrr::map(seq_along(de_data_list()),
+                 ~ eval(str2expression(paste0("input$custom_name_de_", .x)))),
+      purrr::map(seq_along(can_data_list()),
+                 ~ eval(str2expression(paste0("input$custom_name_can_", .x))))
+    )
+  })
 
   ## Getting all the data
   all_data_list <- reactive({
@@ -252,6 +324,14 @@ server <- function(input, output, session){
       combine_chrom_length()
   })
 
+  plot_nrows <- reactive({
+    res <- input$nrows
+    print(res)
+    if(missing(res) | is.na(res)) res <- NULL
+
+    res
+  })
+
   hidecan_plot <- reactive({
     ## wait for at list one input dataset
     req(any_data())
@@ -263,41 +343,49 @@ server <- function(input, output, session){
         purrr::map(apply_threshold, input$score_thr_de, input$log2fc_thr_de),
       can_data_list() |>
         purrr::map(apply_threshold)
-    )
+    ) |>
+      setNames(get_custom_names())
 
-    create_hidecan_plot(x, chrom_length())
+    create_hidecan_plot(x,
+                        chrom_length(),
+                        colour_genes_by_score = input$colour_genes_by_score,
+                        title = input$title,
+                        subtitle = input$subtitle,
+                        n_rows = plot_nrows(),
+                        n_cols = input$ncols,
+                        legend_position = input$legend_position,
+                        point_size = input$point_size,
+                        label_size = input$label_size,
+                        label_padding = input$label_padding)
   })
 
   ## Display the download button
   output$download_params <- renderUI({
     req(any_data())
 
-    tabPanel(
-      title = "Download",
+    #h3("Download"),
 
-      h3("Download"),
-
-      ## Parameters for downloading the data
-      #download_params
-      fluidRow(
-        column(
-          3,
-          selectInput("download_format", "Format", c("png", "pdf"))
-        ),
-        column(
-          3,
-          numericInput("download_plot_width", "Width (in)", value = 16, min = 0, max = Inf)
-        ),
-        column(
-          3,
-          numericInput("download_plot_height", "Height (in)", value = 10, min = 0, max = Inf)
-        ),
-        column(
-          3,
-          style = "margin-top: 25px;",
-          downloadButton("download_plot")
-        )
+    ## Parameters for downloading the data
+    #download_params
+    fluidRow(
+      column(
+        3,
+        selectInput("download_format", "Format", c("png", "pdf"))
+      ),
+      column(
+        3,
+        numericInput("download_plot_width", "Width (in)", value = 16, min = 0, max = Inf)
+      ),
+      column(
+        3,
+        numericInput("download_plot_height", "Height (in)", value = 10, min = 0, max = Inf)
+      ),
+      column(
+        3,
+        style = "margin-top: 25px;",
+        downloadButton("download_plot")
       )
+
     )
 
   })
@@ -327,7 +415,6 @@ server <- function(input, output, session){
       paste0("hidecan_plot_", Sys.Date(), ".", input$download_format)
     },
     content = function(file){
-      print(file)
       ggplot2::ggsave(file,
                       hidecan_plot(),
                       device = input$download_format,
